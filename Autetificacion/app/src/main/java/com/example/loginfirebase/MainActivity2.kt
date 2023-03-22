abd4e67c8a13222e81ff690ceb44a3178e699e15
Yossec SA <yosec.arratea22@gmail.com>
2023-03-22 00:04:10 -0500

    codigo para cerrar sesion, usando el menu

--- Autetificacion/app/src/main/java/com/example/loginfirebase/MainActivity2.kt
+++ Autetificacion/app/src/main/java/com/example/loginfirebase/MainActivity2.kt
@@ -1,14 +1,24 @@
 package com.example.loginfirebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity2 : AppCompatActivity() {
+
+    private  lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        firebaseAuth = Firebase.auth
+
     }
 
     //para cuando retrocedamos no salga del menu principal
@@ -27,10 +37,33 @@ class MainActivity2 : AppCompatActivity() {
     //super.onCreateOptionsMenu(menu)
     }
 
-    
+    // ´para dar funcionalidad a los elementos del menu
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId)
+        {
+            R.id.menu_buscar ->{
+                Toast.makeText(baseContext,"Buscar Info", Toast.LENGTH_LONG).show()
+            }
+            R.id.menu_salir ->{
+                signOut()
+            }
+        }
+
+
+        return super.onOptionsItemSelected(item)
+    }
+
 
     override fun onBackPressed() {
         return
+    }
+
+
+    private fun signOut(){
+        firebaseAuth.signOut()
+        Toast.makeText(baseContext,"Se cerro exitosamente la cuenta", Toast.LENGTH_LONG).show()
+        val i = Intent(this,MainActivity::class.java)
+        startActivity(i)
     }
 
 }
