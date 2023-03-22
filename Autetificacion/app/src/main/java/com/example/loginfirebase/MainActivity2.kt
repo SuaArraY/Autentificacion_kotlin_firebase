7b7b837506abcb9321060d98971c524547f87a21
Yossec SA <yosec.arratea22@gmail.com>
2023-03-21 23:40:01 -0500

    para cancelar el boton retroceder

--- Autetificacion/app/src/main/java/com/example/loginfirebase/MainActivity2.kt
+++ Autetificacion/app/src/main/java/com/example/loginfirebase/MainActivity2.kt
@@ -8,4 +8,19 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
     }
+
+    //para cuando retrocedamos no salga del menu principal
+    //pasos
+    //CODE - OVERRIDE METHODS
+    //y buscamos  OnBackPressed
+
+    //override fun onBackPressed() {
+       // super.onBackPressed()             - el super permite retroceder con el boton retroceder
+    //}
+
+
+    override fun onBackPressed() {
+        return
+    }
+
 }
