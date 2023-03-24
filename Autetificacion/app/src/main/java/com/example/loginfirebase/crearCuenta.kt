6e8de6e476e6270a470a93393a40b7422acca2f0
Yossec SA <yosec.arratea22@gmail.com>
2023-03-24 00:32:07 -0500

    codigo para tomar foto y poner en la imageView

--- Autetificacion/app/src/main/java/com/example/loginfirebase/crearCuenta.kt
+++ Autetificacion/app/src/main/java/com/example/loginfirebase/crearCuenta.kt
@@ -1,8 +1,13 @@
 package com.example.loginfirebase
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
@@ -10,10 +15,13 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import org.w3c.dom.Text
 
+@Suppress("DEPRECATION")
 class crearCuenta : AppCompatActivity() {
 
     //variable que conecta a Firebase
     private lateinit var firebaseAuth: FirebaseAuth
+    private val REQUEST_IMAGE_CAPTURE = 1
+    private lateinit var mImageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +31,14 @@ class crearCuenta : AppCompatActivity() {
         val txtContra2 : TextView = findViewById(R.id.txtContraCC2)
         val txtCorreo: TextView = findViewById(R.id.txtCorreoCC)
         val btnCrearCuentaNueva : Button = findViewById(R.id.btnRegistrarCC)
+        val btnFoto : Button = findViewById(R.id.btnImagenCC)
+        val btnGaleria = findViewById<Button>(R.id.btnescogerImg)
+         mImageView = findViewById(R.id.imgCrearCuenta)
+
+        btnFoto.setOnClickListener(){
+            dispatchTakePictureIntent()
+        }
+
 
         btnCrearCuentaNueva.setOnClickListener(){
 
@@ -57,4 +73,25 @@ crearAccount(txtCorreo.text.toString(),txtContra.text.toString())
 
             }
     }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    private fun dispatchTakePictureIntent() {
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val extras = data?.extras
+            val imageBitmap = extras?.get("data") as Bitmap
+            mImageView.setImageBitmap(imageBitmap)
+        }
+    }
+
+
 }
