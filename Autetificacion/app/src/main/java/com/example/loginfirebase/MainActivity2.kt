d93d3f60c79b78396558b6f71175afe8cf07750e
Yossec SA <yosec.arratea22@gmail.com>
2023-03-24 02:52:43 -0500

    falta traer la imagen del usuario respectivo

--- Autetificacion/app/src/main/java/com/example/loginfirebase/MainActivity2.kt
+++ Autetificacion/app/src/main/java/com/example/loginfirebase/MainActivity2.kt
@@ -10,12 +10,15 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.*
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ktx.database
 
 
@@ -23,19 +26,21 @@ import com.google.firebase.database.ktx.database
 @Suppress("DEPRECATION", "NAME_SHADOWING")
 class MainActivity2 : AppCompatActivity() {
 
-    private  lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var textViewUsername: TextView
 
+    private lateinit var databaseRef: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
 
-        val btnCamara : Button = findViewById(R.id.btnCamara)
-        val btnBaseDatos : Button = findViewById(R.id.btnCrearBaseDatos)
+        val btnCamara: Button = findViewById(R.id.btnCamara)
+        val btnBaseDatos: Button = findViewById(R.id.btnCrearBaseDatos)
         val btnCamra2 = findViewById<Button>(R.id.btnCamara2)
 
-        btnCamra2.setOnClickListener(){
+        /* btnCamra2.setOnClickListener(){
             val i = Intent(this, camara2::class.java)
             startActivity(i)
         }
@@ -47,9 +52,31 @@ class MainActivity2 : AppCompatActivity() {
         btnCamara.setOnClickListener(){
             val i = Intent(this, tomarFoto::class.java)
             startActivity(i)
-        }
-        
+        }*/
+
         firebaseAuth = Firebase.auth
+        databaseRef = Firebase.database.reference
+
+        //obtener el usuario actual y mostrar su nombre en un TextView
+        val currentUser = firebaseAuth.currentUser
+        val userId = currentUser?.uid
+        val txtUsername: TextView = findViewById(R.id.textView2)
+        databaseRef.child("users").child(userId.toString()).child("name")
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val name = snapshot.value.toString()
+                    txtUsername.text = name
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    Log.d("Firebase", "Error al obtener el nombre del usuario")
+                }
+            })
+
+        //configurar la imagen de perfil del usuario
+        val imgProfile: ImageView = findViewById(R.id.imageView2)
+        val photoUrl = currentUser?.photoUrl
+         Glide.with(this).load(photoUrl).into(imgProfile)
 
     }
 
